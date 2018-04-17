172fed389cdea9a3b7dc8b7fdec3509a5911736d
palvarezq_ext <palvarezq.ext@acciona.com>
2018-04-17 13:05:27 +0200

    New project test

---
+++ WebApplicationTestGitHub2015/WebApplicationTestGitHub2015/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationTestGitHub2015.Startup))]
+namespace WebApplicationTestGitHub2015
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
